5cc48f0608737453128658d3e01ecbb27df2451d
liushengzhong <sl29@illinois.edu>
2020-09-21 01:06:22 -0500

    Add implementations by Franck.

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import time
 from argparse import ArgumentParser
 
 import tensorflow as tf
+import numpy as np
 from tqdm import tqdm
 
 from data import dataset_type
@@ -23,9 +24,9 @@ def create_experiment_name(model_name, main_config, model_config):
     experiment_name = '{}_{}'.format(model_name, main_config['PARAMS']['embedding_size'])
     if model_name == model_type.ModelType.rnn.name:
         experiment_name += ("_" + model_config['PARAMS']['cell_type'])
-    
+
     experiment_name += ("_" + main_config['PARAMS']['loss_function'])
-    
+
     return experiment_name
 
 
@@ -39,25 +40,25 @@ def train(
     main_cfg = MainConfig(main_config)
     model = MODELS[model_name]
     dataset = dataset_type.get_dataset(dataset_name)
-    
+
     train_data = dataset.train_set_pairs()
     vectorizer = DatasetVectorizer(
         model_dir=main_cfg.model_dir,
         char_embeddings=main_cfg.char_embeddings,
         raw_sentence_pairs=train_data,
     )
-    
+
     dataset_helper = Dataset(vectorizer, dataset, main_cfg.batch_size)
     max_sentence_len = vectorizer.max_sentence_len
     vocabulary_size = vectorizer.vocabulary_size
-    
+
     train_mini_sen1, train_mini_sen2, train_mini_labels = dataset_helper.pick_train_mini_batch()
     train_mini_labels = train_mini_labels.reshape(-1, 1)
-    
+
     test_sentence1, test_sentence2 = dataset_helper.test_instances()
     test_labels = dataset_helper.test_labels()
     test_labels = test_labels.reshape(-1, 1)
-    
+
     num_batches = dataset_helper.num_batches
     model = model(
         max_sentence_len,
@@ -74,7 +75,7 @@ def train(
         allow_soft_placement=True,
         log_device_placement=main_cfg.log_device_placement,
     )
-    
+
     with tf.Session(config=config) as session:
         global_step = 0
         init = tf.global_variables_initializer()
@@ -86,28 +87,28 @@ def train(
             session.graph,
         )
         model_evaluator = ModelEvaluator(model, session)
-        
+
         metrics = {'acc': 0.0}
         time_per_epoch = []
-        
+
         log('Training model for {} epochs'.format(main_cfg.num_epochs))
         for epoch in tqdm(range(main_cfg.num_epochs), desc='Epochs'):
             start_time = time.time()
-            
+
             train_sentence1, train_sentence2 = dataset_helper.train_instances(shuffle=True)
             train_labels = dataset_helper.train_labels()
-            
+
             train_batch_helper = BatchHelper(
                 train_sentence1,
                 train_sentence2,
                 train_labels,
                 main_cfg.batch_size,
             )
-            
+
             # small eval set for measuring dev accuracy
             dev_sentence1, dev_sentence2, dev_labels = dataset_helper.dev_instances()
             dev_labels = dev_labels.reshape(-1, 1)
-            
+
             tqdm_iter = tqdm(range(num_batches), total=num_batches, desc="Batches", leave=False,
                              postfix=metrics)
             for batch in tqdm_iter:
@@ -120,7 +121,7 @@ def train(
                     model.labels: labels_batch,
                 }
                 loss, _ = session.run([model.loss, model.opt], feed_dict=feed_dict_train)
-                
+
                 if batch % main_cfg.eval_every == 0:
                     feed_dict_train = {
                         model.x1: train_mini_sen1,
@@ -128,20 +129,20 @@ def train(
                         model.is_training: False,
                         model.labels: train_mini_labels,
                     }
-                    
+
                     train_accuracy, train_summary = session.run(
                         [model.accuracy, model.summary_op],
                         feed_dict=feed_dict_train,
                     )
                     log_saver.log_train(train_summary, global_step)
-                    
+
                     feed_dict_dev = {
                         model.x1: dev_sentence1,
                         model.x2: dev_sentence2,
                         model.is_training: False,
                         model.labels: dev_labels
                     }
-                    
+
                     dev_accuracy, dev_summary = session.run(
                         [model.accuracy, model.summary_op],
                         feed_dict=feed_dict_dev,
@@ -153,22 +154,22 @@ def train(
                         loss='{:.2f}'.format(float(loss)),
                         epoch=epoch
                     )
-                
+
                 if global_step % main_cfg.save_every == 0:
                     model_saver.save(session, global_step=global_step)
-            
+
             model_evaluator.evaluate_dev(
                 x1=dev_sentence1,
                 x2=dev_sentence2,
                 labels=dev_labels,
             )
-            
+
             end_time = time.time()
             total_time = timer(start_time, end_time)
             time_per_epoch.append(total_time)
-            
+
             model_saver.save(session, global_step=global_step)
-        
+
         model_evaluator.evaluate_test(test_sentence1, test_sentence2, test_labels)
         model_evaluator.save_evaluation(
             model_path='{}/{}'.format(
@@ -188,19 +189,19 @@ def predict(
 ):
     model = MODELS[model]
     main_cfg = MainConfig(main_config)
-    
+
     # model_dir = str(main_config['DATA']['model_dir'])
-    
+
     vectorizer = DatasetVectorizer(
         model_dir=main_cfg.model_dir,
         char_embeddings=main_cfg.char_embeddings,
     )
-    
+
     max_doc_len = vectorizer.max_sentence_len
     vocabulary_size = vectorizer.vocabulary_size
-    
+
     model = model(max_doc_len, vocabulary_size, main_config, model_config)
-    
+
     with tf.Session() as session:
         saver = tf.train.Saver()
         last_checkpoint = tf.train.latest_checkpoint(
@@ -210,66 +211,100 @@ def predict(
             )
         )
         saver.restore(session, last_checkpoint)
+        # while True:
+        #     x1 = input('First sentence:')
+        #     x2 = input('Second sentence:')
+        #     x1_sen = vectorizer.vectorize(x1)
+        #     x2_sen = vectorizer.vectorize(x2)
+        #
+        #     feed_dict = {model.x1: x1_sen, model.x2: x2_sen, model.is_training: False}
+        #     prediction = session.run([model.temp_sim], feed_dict=feed_dict)
+        #     print(prediction)
+
+        # by Franck
         while True:
             x1 = input('First sentence:')
             x2 = input('Second sentence:')
+            x3 = input('Label:')
             x1_sen = vectorizer.vectorize(x1)
             x2_sen = vectorizer.vectorize(x2)
-            
-            feed_dict = {model.x1: x1_sen, model.x2: x2_sen, model.is_training: False}
+            feed_dict = {model.x1: x1_sen, model.x2: x2_sen, model.is_training: False,
+                         model.labels: np.array([[int(x3)]])}
             prediction = session.run([model.temp_sim], feed_dict=feed_dict)
+            print("Prediction:")
             print(prediction)
+            print("Loss:")
+            print(session.run([model.loss], feed_dict=feed_dict))
+            # print(session.run([tf.convert_to_tensor(x1_sen)], feed_dict=feed_dict))
+            # print(session.run([tf.shape(model.embedded_x1)], feed_dict=feed_dict))
+            # print(session.run([tf.shape(tf.reshape(model.embedded_x1, [-1, 64]))], feed_dict=feed_dict))
+            gradients_1 = tf.gradients(model.loss, model.embedded_x1)
+            gradients_2 = tf.gradients(model.loss, model.embedded_x2)
+            # print(session.run([tf.reduce_mean(tf.math.multiply(tf.reshape(gradients_1, [-1, 64]), tf.reshape(model.embedded_x1, [-1, 64])), 1)], feed_dict=feed_dict))
+            print(session.run([tf.argsort(tf.reduce_mean(
+                tf.math.multiply(tf.reshape(gradients_1, [-1, 64]), tf.reshape(model.embedded_x1, [-1, 64])), 1),
+                                          axis=-1, direction='ASCENDING', stable=False, name=None)],
+                              feed_dict=feed_dict))
+            # print(session.run([tf.reduce_mean(tf.math.multiply(tf.reshape(gradients_2, [-1, 64]), tf.reshape(model.embedded_x1, [-1, 64])), 1)], feed_dict=feed_dict))
+            print(session.run([tf.argsort(tf.reduce_mean(
+                tf.math.multiply(tf.reshape(gradients_2, [-1, 64]), tf.reshape(model.embedded_x2, [-1, 64])), 1),
+                                          axis=-1, direction='ASCENDING', stable=False, name=None)],
+                              feed_dict=feed_dict))
+            break
 
 
 def main():
     parser = ArgumentParser()
-    
+
     parser.add_argument(
         'mode',
         choices=['train', 'predict'],
         help='pipeline mode',
+        default='predict'
     )
-    
+
     parser.add_argument(
         'model',
         choices=['rnn', 'cnn', 'multihead'],
         help='model to be used',
+        default='multihead'
     )
-    
+
     parser.add_argument(
         'dataset',
         choices=['QQP', 'SNLI', 'ANLI'],
         nargs='?',
         help='dataset to be used',
+        default='SNLI'
     )
-    
+
     parser.add_argument(
         '--experiment_name',
         required=False,
         help='the name of run experiment',
     )
-    
+
     parser.add_argument(
         '--gpu',
         default='0',
         help='index of GPU to be used (default: %(default))',
     )
-    
+
     args = parser.parse_args()
     if 'train' in args.mode:
         if args.dataset is None:
             parser.error('Positional argument [dataset] is mandatory')
     set_visible_gpu(args.gpu)
-    
+
     main_config = init_config()
     model_config = init_config(args.model)
-    
+
     mode = args.mode
-    
+
     experiment_name = args.experiment_name
     if experiment_name is None:
         experiment_name = create_experiment_name(args.model, main_config, model_config)
-    
+
     if 'train' in mode:
         train(main_config, model_config, args.model, experiment_name, args.dataset)
     else:
